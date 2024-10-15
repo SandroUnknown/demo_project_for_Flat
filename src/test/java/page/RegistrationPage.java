3b20ebe0dc74ec3c81ad8c9c34aac93983a7cdfa
Alexander Petyukov <sandrounknown1991@gmail.com>
2024-10-15 22:36:49 +0300

    rewrite code

--- src/test/java/page/RegistrationPage.java
+++ src/test/java/page/RegistrationPage.java
@@ -2,7 +2,6 @@ package page;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
-import io.qameta.allure.Step;
 import page.components.CalendarComponent;
 import page.components.TableResponsive;
 
@@ -29,57 +28,48 @@ public class RegistrationPage {
             stateCityWrapper    = $("#stateCity-wrapper"),
             submitInput         = $("#submit");
 
-    @Step("Открываем форму.")
     public RegistrationPage openPage() {
         open("/automation-practice-form");
         return this;
     }
 
-    @Step("Убираем баннеры со страницы.")
     public RegistrationPage removeBanners() {
         executeJavaScript("$('#fixedban').remove()");
         executeJavaScript("$('footer').remove()");
         return this;
     }
 
-    @Step("Вводим имя.")
     public RegistrationPage setFirstName(String value) {
         firstNameInput.scrollTo().setValue(value);
         return this;
     }
 
-    @Step("Вводим фамилию.")
     public RegistrationPage setLastName(String value) {
         lastNameInput.scrollTo().setValue(value);
         return this;
     }
 
-    @Step("Вводим email.")
     public RegistrationPage setUserEmail(String value) {
         userEmailInput.scrollTo().setValue(value);
         return this;
     }
 
-    @Step("Выбираем пол.")
     public RegistrationPage setGender(String value) {
         genderInput.scrollTo().$(byText(value)).click();
         return this;
     }
 
-    @Step("Вводим номер телефона.")
     public RegistrationPage setUserNumber(String value) {
         userNumberInput.scrollTo().setValue(value);
         return this;
     }
 
-    @Step("Выбираем дату рождения.")
     public RegistrationPage setDateOfBirth(String day, String month, String year) {
         dateOfBirth.scrollTo().click();
         new CalendarComponent().setDate(day, month, year);
         return this;
     }
 
-    @Step("Выбираем предметы.")
     public RegistrationPage setSubjects(List<String> values) {
         subjectsInput.scrollTo();
         for (String value : values) {
@@ -88,7 +78,6 @@ public class RegistrationPage {
         return this;
     }
 
-    @Step("Выбираем увлечения.")
     public RegistrationPage setHobbies(List<String> values) {
         hobbiesInput.scrollTo();
         for (String value : values) {
@@ -97,34 +86,29 @@ public class RegistrationPage {
         return this;
     }
 
-    @Step("Загружаем картинку.")
     public RegistrationPage setPicture(String value) {
         if (Configuration.browser.equals("firefox")) return this;
         pictureInput.scrollTo().uploadFromClasspath(value);
         return this;
     }
 
-    @Step("Выбираем адрес.")
     public RegistrationPage setCurrentAddress(String value) {
         addressCurrentInput.scrollTo().setValue(value);
         return this;
     }
 
-    @Step("Выбираем штат.")
     public RegistrationPage setState(String value) {
         stateInput.scrollTo().click();
         stateCityWrapper.$(byText(value)).click();
         return this;
     }
 
-    @Step("Выбираем город.")
     public RegistrationPage setCity(String value) {
         cityInput.scrollTo().click();
         stateCityWrapper.$(byText(value)).click();
         return this;
     }
 
-    @Step("Нажимаем на кнопку 'submit'.")
     public void clickSubmit() {
         submitInput.scrollTo().click();
     }
